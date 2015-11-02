f2ff24c20316213aef075d5247466a2ab799220f
Vaibhav Yadaram <yadaram.vaibhav@gmail.com>
2015-11-02 01:26:59 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("door9").onmousedown = function() 
+{alert("Don't mess with Ye");
+    alert("Play again?");
+    setTimeout(function(){
+   window.location.reload(1);
+}, 2000);
+};
